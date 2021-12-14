bd025e801acbf3d22b9d99edd82c89e27ab8d9f8
MaxValeev <valeevphoto@gmail.com>
2021-12-14 16:41:46 +0500

    Base scripts and folder structure added

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private CardContent cardContent;
+    
+    //public void SetContent(GameObject contentPrefab)
+}
---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardData
+{
+    [SerializeField] private string identifier;
+    [SerializeField] private Sprite cardSprite;
+
+    //
+}
