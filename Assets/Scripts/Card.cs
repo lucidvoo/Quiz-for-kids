d24c7ef9c41faafbb74a63da3aaba9bc034b08b1
lucidvoo <90751594+lucidvoo@users.noreply.github.com>
2021-12-19 22:49:12 +0500

    pick random content and right answer

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,11 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    [SerializeField] private CardContent cardContent;
+    [SerializeField] private SpriteRenderer contentSpriteRenderer;
+    [SerializeField] private RectTransform contentRect;
 
-    //public void SetContent(GameObject contentPrefab)
+    private string contentIdentifier;
 
+    public string ContentIdentifier => contentIdentifier;
+
+    public void SetContent(CardData cardData)
+    {
+        this.contentIdentifier = cardData.Identifier;
+        contentSpriteRenderer.sprite = cardData.CardSprite;
+
+        SetContentProperSize();
+    }
+
+
+    // изменить скейл спрайта контента так, чтобы он вписался в указанный на префабе прямоугольник
+    private void SetContentProperSize()
+    {
+        contentSpriteRenderer.transform.localScale = Vector3.one;
+
+        // определить аспект-ратио спрайта и ректа
+        float spriteAspect = contentSpriteRenderer.bounds.size.x / contentSpriteRenderer.bounds.size.y;
+        float rectAspect = contentRect.rect.width / contentRect.rect.height;
+        float scaleToSet;
+
+        // посчитать нужный скейл по ширине, либо по высоте
+        if (spriteAspect > rectAspect)
+        {
+            scaleToSet = (contentRect.rect.width) / contentSpriteRenderer.bounds.size.x;
+        }
+        else
+        {
+            scaleToSet = contentRect.rect.height / contentSpriteRenderer.bounds.size.y;
+        }
+
+        // назначить скейл
+        contentSpriteRenderer.transform.localScale *= scaleToSet;
+    }
 }
--- Assets/Scripts/CardGrid.cs
+++ Assets/Scripts/CardGrid.cs
@@ -15,6 +15,7 @@ public class CardGrid : MonoBehaviour
     private Dictionary<int, HashSet<string>> usedRightAnswers = new Dictionary<int, HashSet<string>>(2);
     private int currentLevelInd = 0;
     private int currentCardSetInd;
+    private string rightAnswer;
     private Card[,] cards;
 
     void Start()
@@ -33,35 +34,66 @@ public class CardGrid : MonoBehaviour
                                    transform);
 
         SetupCardContents();
+        PickRightAnswer();
     }
 
 
-    // выбор случайного набора контента, выставление случайных значений из набора в ячейки, выбор правильного варианта.
+    // выбор случайного набора контента, выставление случайных значений из набора в ячейки.
     private void SetupCardContents()
     {
         // случайно выбрать набор карточек
         currentCardSetInd = UnityEngine.Random.Range(0, cardSets.Length);
 
-        // случайно выбрать массив карточек, которые мы используем
+        // случайно выбрать массив карточек из набора, которые мы используем в уровне
         if (cardSets[currentCardSetInd].Cards.Length < cards.Length)
         {
             Debug.LogError("Один из наборов карточек слишком мал. Добавьте элементы или уменьшите размер уровня");
         }
 
+        List<CardData> fullCardSet = new List<CardData>(cardSets[currentCardSetInd].Cards.Length);
+        for (int i = 0; i < cardSets[currentCardSetInd].Cards.Length; i++)
+        {
+            fullCardSet.Add(cardSets[currentCardSetInd].Cards[i]);
+        }
+
+        List<CardData> selectedCardSet = new List<CardData>(cards.Length);
+        for (int i = 0; i < cards.Length; i++)
+        {
+            int randIndex = UnityEngine.Random.Range(0, fullCardSet.Count);
+            selectedCardSet.Add(fullCardSet[randIndex]);
+            fullCardSet.RemoveAt(randIndex);
+        }
+        fullCardSet = null; // название больше не отражает содержимого массива
+
+        // записать найденный массив в ячейки
+        int ind = 0;
+        foreach (Card card in cards)
+        {
+            card.SetContent(selectedCardSet[ind]);
+            ind++;
+        }
+    }
 
 
-        // из рабочего массива карточек случайно принять одну за верное решение с проверкой, чтобы такой не было ранее
-
-
-
-
-
-
+    // из рабочего массива карточек случайно принять одну за верное решение с проверкой, чтобы такой не было ранее
+    private void PickRightAnswer()
+    {
         if (!usedRightAnswers.ContainsKey(currentCardSetInd))
         {
             usedRightAnswers.Add(currentCardSetInd, new HashSet<string>());
         }
+        else
+        {
+            string identifierToCheck;
+            do
+            {
+                int r = UnityEngine.Random.Range(0, cards.GetLength(0));
+                int c = UnityEngine.Random.Range(0, cards.GetLength(1));
+                identifierToCheck = cards[r, c].ContentIdentifier;
+            } while (usedRightAnswers[currentCardSetInd].Contains(identifierToCheck));
 
-
+            rightAnswer = identifierToCheck;
+            usedRightAnswers[currentCardSetInd].Add(rightAnswer);
+        }
     }
 }
